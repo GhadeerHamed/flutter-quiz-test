103c7af8a51479be85b0192df6cad9e03c5e53d7
GhadeerHamed <eng.gh.a.h93@gmail.com>
2025-10-29 23:52:00 +0300

    Add StartScreen widget for quiz application
    - Implemented StartScreen as a StatelessWidget.
    - Added an image, title text, and a start quiz button.
    - Styled the button with an icon and custom colors.

---
+++ lib/start_screen.dart
@@ -0,0 +1,30 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset("assets/images/quiz-logo.png"),
+          SizedBox(height: 20),
+          const Text(
+            "Learn Flutter the fun way!.",
+            style: TextStyle(fontSize: 24, color: Colors.white),
+          ),
+          SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+            icon: const Icon(Icons.arrow_right_alt),
+            iconAlignment: IconAlignment.end,
+            label: const Text("start Quiz"),
+          ),
+        ],
+      ),
+    );
+  }
+}
