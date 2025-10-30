9a3d14728d82c4d585c9a1ad442f1e9cdac94d95
GhadeerHamed <eng.gh.a.h93@gmail.com>
2025-10-30 15:02:02 +0300

    Implement answer shuffling in QuestionsScreen for randomized answer display

--- lib/models/quiz_question.dart
+++ lib/models/quiz_question.dart
@@ -3,4 +3,10 @@ class QuizQuestion {
   final List<String> answers;
 
   const QuizQuestion(this.question, this.answers);
+
+  List<String> getShuffledAnswers() {
+    final shuffledAnswers = List<String>.from(answers);
+    shuffledAnswers.shuffle();
+    return shuffledAnswers;
+  }
 }
